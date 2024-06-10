81e91558e26583645208080adba37ecd02d9550a
tessawernars <tessa.wernars@student.uva.nl>
2024-06-10 10:03:55 +0200

    pressure plate activation improved

--- scripts/interactables/pressure_plate.gd
+++ scripts/interactables/pressure_plate.gd
@@ -1,18 +1,18 @@
 extends Node3D
 
-var activated = false
+var player = null
 
 # Detect when body entered the area
 func _on_area_3d_body_entered(body):
-	if body is CharacterBody3D and !activated:
-		$Door/AnimationPlayer.play("Door Sliding")
-		activated = true
+	if body is CharacterBody3D and player == null:
+		player = body
+		player.activate_door_open()
 
 # Detect when body exited the area
 func _on_area_3d_body_exited(body):
-	if body is CharacterBody3D and activated:
-		$Door/AnimationPlayer.play_backwards("Door Sliding")
-		activated = false
+	if body is CharacterBody3D and player != null:
+		player.activate_door_close()
+		player = null
 
 # Change the scale of the pressure plate
 func scale_pressure_plate(scaling_factor):
@@ -21,7 +21,7 @@ func scale_pressure_plate(scaling_factor):
 # Change to position of the pressure plate
 func transform_pressure_plate(x, y, z):
 	$PressurePlate.position = Vector3(x, y, z)
-	
+
 # Change the scale of the door
 func scale_door(scaling_factor):
 	$Door.scale *= scaling_factor
