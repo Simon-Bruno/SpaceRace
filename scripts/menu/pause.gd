5147345131da5cf70e570d1d0507367690bd9ea7
Milou <milouhermes@outlook.com>
2024-06-11 15:46:39 +0200

    Pause menu appears when esc, but doesnt go away

--- scripts/menu/pause.gd
+++ scripts/menu/pause.gd
@@ -1,38 +1,27 @@
-extends Node
-
-@onready var pause_menu = $Pause
-
+extends Control
 
 func _process(delta):
-	if Input.is_action_just_pressed("pause"):
-		_toggle_pause_menu()
+	if Input.is_action_pressed("pause") and !get_tree().paused:
+		_pause_game()
+	elif Input.is_action_pressed("pause") and get_tree().paused:
+		_resume_game()
 
-
-func _toggle_pause_menu():
-	if get_tree().paused:
-		pause_menu.hide_menu()
-	else:
-		pause_menu.show_menu()
+func _pause_game():
+	get_tree().paused = true
+	self.visible = true
+	print("Pausing game")
 
 func _resume_game():
 	get_tree().paused = false
-	
-	
-func _pause_game():
-	get_tree().paused = true
-	
+	self.visible = false
+	print("Resuming game")
 
-func _on_resume_pressed():
+func _on_resume_button_pressed():
+	print("Resume button pressed")
 	_resume_game()
 
+func _on_settings_button_pressed():
+	print("Settings button pressed")
 
-func _on_settings_pressed():
-	print("Settings page TBC")
-
-
-func _on_titlescreen_pressed():
-	print("TitleScreen TBC")
-
-
-func _on_quit_pressed():
-	get_tree().quit()
+func _on_titlescreen_button_pressed():
+	print("Titlescreen button pressed")
