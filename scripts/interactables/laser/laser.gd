91ce07829d322b1819c02166fd0f2d1231f7c3d8
Yifan Chen <yifan.chen2@student.uva.nl>
2024-06-11 18:21:30 +0200

    chore: updated names to be more clear

--- scripts/interactables/laser/laser.gd
+++ scripts/interactables/laser/laser.gd
@@ -12,7 +12,7 @@ func _on_area_3d_body_entered(body):
 		target = body
 
 func _on_area_3d_body_exited(body):
-	if body.is_in_group("Players"):	
+	if body.is_in_group("Players"):
 		target = null
 		# make sure first tick always does dmg
 		damage_time = damage_delay
