53313ec50a62c1e3195bc52e046f0ff9959acd95
wesselpierik <wessel.pierik@student.uva.nl>
2024-06-11 17:07:20 +0200

    feat: The walls now get represented as blocks and randomly spawned (but no other items get spawned)

--- scripts/world/rms_parser.gd
+++ scripts/world/rms_parser.gd
@@ -2,20 +2,19 @@ extends Node
 
 
 var wall_test = [ {"set_min_distance": "5", "set_max_distance": "8", "length": "6", "length_variation": "2" }, { "set_min_distance": "4", "set_max_distance": "6", "length": "3", "length_variation": "1" } ]
-var object_test = { 1: { "set_min_distance": "8", "set_max_distance": "10", "set_length": "3", "type": "LASER" }, 2: { "set_min_distance": "17", "set_max_distance": "20", "type": "ITEM" }, 3: { "set_min_distance": "4", "set_max_distance": "9", "type": "ITEM" } }
-var enemy_test = { 1: { "set_min_distance": "5", "set_max_distance": "8", "set_group_size": "3", "loose_grouping": true } }
+var object_test = [ { "set_min_distance": "8", "set_max_distance": "10", "set_length": "3", "type": "LASER" }, { "set_min_distance": "17", "set_max_distance": "20", "type": "ITEM" }, { "set_min_distance": "4", "set_max_distance": "9", "type": "ITEM" } ]
+var enemy_test = [ { "set_min_distance": "5", "set_max_distance": "8", "set_group_size": "3", "loose_grouping": true } ]
 
 func _ready():
 	var rms_dict = parse_file("res://files/random_map_scripts/test.rms")
 	print(rms_dict)
-	assert(rms_dict['walls'] == wall_test)
+	assert(rms_dict['walls'] == wall_test, "The walls were not parsed correctly")
 	print("The walls get parsed correctly")
 	assert(rms_dict['objects'] == object_test, "The objects were not parsed correctly")
 	print("The objects get parsed correctly")
-	assert(rms_dict['enemies'] == enemy_test)
+	assert(rms_dict['enemies'] == enemy_test, "The enemies were not parsed correctly")
 	print("The enemies got parsed correctly")
 	print("All tests passed")
-
 
 # Called when the node enters the scene tree for the first time.
 func parse_file(filepath : String) -> Dictionary:
@@ -28,14 +27,14 @@ func parse_file(filepath : String) -> Dictionary:
 			dict['walls'] = []
 			wall_parser(section, dict['walls'])
 		elif section.begins_with("OBJECTS_GENERATION"):
-			dict['objects'] = {}
+			dict['objects'] = []
 			object_parser(section, dict['objects'])
 		elif section.begins_with("ENEMY_GENERATION"):
-			dict['enemies'] = {}
+			dict['enemies'] = []
 			enemies_parser(section, dict['enemies'])
 	return dict
-	
-func segment_to_dict(segment : String):
+
+func segment_to_dict(segment : String) -> Dictionary:
 	var dict = {}
 	if not segment.begins_with("{"):
 		return {}
@@ -60,7 +59,7 @@ func wall_parser(section: String, wall_dict : Array) -> void:
 			continue
 		wall_dict.append(parsed_wall)
 		
-func object_parser(section: String, object_dict : Dictionary) -> void:
+func object_parser(section: String, object_dict : Array) -> void:
 	var objects = section.split('create_object')
 	for i in objects.size():
 		# Get an object from the list
@@ -80,14 +79,13 @@ func object_parser(section: String, object_dict : Dictionary) -> void:
 		# Set the type of object
 		parsed_object['type'] = type
 		# Set the object in the dictionary of objects
-		object_dict[i] = parsed_object
+		object_dict.append(parsed_object)
 
-func enemies_parser(section : String, enemy_dict : Dictionary) -> void:
+func enemies_parser(section : String, enemy_dict : Array) -> void:
 	var mobs = section.split('create_mob')
 	for i in mobs.size():
 		var mob : String = mobs[i].strip_edges()
 		if mob.begins_with("ENEMY_GENERATION"):
 			continue
 		var parsed_mob : Dictionary = segment_to_dict(mob)
-		enemy_dict[i] = parsed_mob
-
+		enemy_dict.append(parsed_mob)
--- scripts/world/roomGeneration.gd
+++ scripts/world/roomGeneration.gd
@@ -2,6 +2,8 @@ extends Node3D
 
 @onready var world = get_parent()
 
+enum {HORIZONTAL, VERTICAL}
+
 var parser = preload("res://scripts/world/rms_parser.gd").new()
 
 var enemy_scene = preload("res://scenes/enemy/enemy.tscn")
@@ -14,13 +16,29 @@ func _ready():
 		var filename = "res://files/random_map_scripts/test.rms"
 		var world_dict : Dictionary = parser.parse_file(filename)
 		fill_room(world_dict)
-		
-func add_walls(wall_list : Array):
+
+func add_walls(wall_list : Array, width : int, height : int, start : Vector3i):
 	for wall in wall_list:
 		var length : int = int(wall['length'])
 		var variation : int = int(wall['length_variation'])
 		length += randi_range(-variation, variation)
-		print(length)
+		var orientation = randi() % 2
+		var x : int = 0
+		var z : int = 0
+		if orientation == HORIZONTAL and width > length:
+			x = randi_range(length / 2, width * 2 - length / 2)
+			z = randi_range(2, height * 2 - 2)
+		else:
+			x = randi_range(2, width * 2 - 2)
+			z = randi_range(length / 2, height * 2 - length / 2)
+			orientation = VERTICAL
+		for i in range(-length / 2, length / 2):
+			var wall_block = box_scene.instantiate()
+			if orientation == HORIZONTAL:
+				wall_block.position = Vector3i(x + i, 2, z)
+			else:
+				wall_block.position = Vector3i(x, 2, z + i)
+			add_child(wall_block, true)
 		
 		
 func add_objects(objects_list):
@@ -28,24 +46,26 @@ func add_objects(objects_list):
 	
 
 func fill_room(world_dict: Dictionary):
-	add_walls(world_dict['walls'])
 	var room = world.room
+	var width : int = room[0]
+	var height : int = room[1]
+	add_walls(world_dict['walls'], width, height, Vector3i(1, 2, 1))
 	
-	var enemy = enemy_scene.instantiate()
-	enemy.position = Vector3i(randi_range(1, room[0] * 2 - 1), randi_range(5, 30), randi_range(1, room[1] * 2 - 1))
-	add_child(enemy, true)
-	
-	var laser = laser_scene.instantiate()
-	laser.position = Vector3i(2, 3, 5)
-	add_child(laser, true)
-	
-	var item = item_scene.instantiate()
-	item.position = Vector3i(randi_range(1, room[0] * 2 - 1), randi_range(3, 10), randi_range(1, room[1] * 2 - 1))
-	add_child(item, true)
-	
-	var box = box_scene.instantiate()
-	box.position = Vector3i(randi_range(1, room[0] * 2 - 1), randi_range(3, 10), randi_range(1, room[1] * 2 - 1))
-	add_child(box)
+	#var enemy = enemy_scene.instantiate()
+	#enemy.position = Vector3i(randi_range(1, room[0] * 2 - 1), randi_range(5, 30), randi_range(1, room[1] * 2 - 1))
+	#add_child(enemy, true)
+	#
+	#var laser = laser_scene.instantiate()
+	#laser.position = Vector3i(2, 3, 5)
+	#add_child(laser, true)
+	#
+	#var item = item_scene.instantiate()
+	#item.position = Vector3i(randi_range(1, room[0] * 2 - 1), randi_range(3, 10), randi_range(1, room[1] * 2 - 1))
+	#add_child(item, true)
+	#
+	#var box = box_scene.instantiate()
+	#box.position = Vector3i(randi_range(1, room[0] * 2 - 1), randi_range(3, 10), randi_range(1, room[1] * 2 - 1))
+	#add_child(box, true)
 
 #func _process(delta):
 	#pass
