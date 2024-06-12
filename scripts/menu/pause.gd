d369fcffcb77428a9050a777f2ccbfd90e99dacb
Milou <milouhermes@outlook.com>
2024-06-12 12:53:52 +0200

    Removed print status

--- scripts/menu/pause.gd
+++ scripts/menu/pause.gd
@@ -18,27 +18,22 @@ func handle_esc_input():
 
 # Will show the pause menu, while the game is still going
 func _pause_game():
-	print("Pausing game")
 	self.visible = true
 	game_status = true
 
 # Hides the pause menu
 func _resume_game():
-	print("Resuming game")
 	self.visible = false
 	game_status = false
 
 func _on_resume_button_pressed():
-	print("Resume button pressed")
 	_resume_game()
 
 func _on_settings_button_pressed():
-	print("Settings button pressed")
 	settings.set_process(true)
 	settings.visible = true
 
 func _on_titlescreen_button_pressed():
-	print("Titlescreen button pressed")
 	Network._on_leave_button_pressed()
 	self.visible = false
 	game_status = false
