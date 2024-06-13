22284b94541e13e264eaea3af8d80b5c4c87f10f
wesselpierik <wessel.pierik@student.uva.nl>
2024-06-13 15:34:49 +0200

    chore: Added a lot of documentation for the generation

--- scripts/world/roomGeneration.gd
+++ scripts/world/roomGeneration.gd
@@ -8,14 +8,15 @@ enum {EMTPY, WALL, ITEM, LASER, ENEMY}
 
 var parser = preload("res://scripts/world/rms_parser.gd").new()
 
-var enemy_scene = preload("res://scenes/enemy/enemy.tscn")
-var laser_scene = preload("res://scenes/interactables/laser.tscn")
-var item_scene = preload("res://scenes/item/item.tscn")
-var box_scene = preload("res://scenes/interactables/moveable_object.tscn")
-var button_scene = preload("res://scenes/interactables/button.tscn")
-var pressure_plate_scene = preload("res://scenes/interactables/pressure_plate.tscn")
-var door_scene = preload("res://scenes/interactables/door.tscn")
-var wall_scene = preload("res://scenes/world/intern_wall.tscn")
+# Some constant scenes to load.
+const enemy_scene = preload("res://scenes/enemy/enemy.tscn")
+const laser_scene = preload("res://scenes/interactables/laser.tscn")
+const item_scene = preload("res://scenes/item/item.tscn")
+const box_scene = preload("res://scenes/interactables/moveable_object.tscn")
+const button_scene = preload("res://scenes/interactables/button.tscn")
+const pressure_plate_scene = preload("res://scenes/interactables/pressure_plate.tscn")
+const door_scene = preload("res://scenes/interactables/door.tscn")
+const wall_scene = preload("res://scenes/world/intern_wall.tscn")
 
 func _ready():
 	if world.generate_room:
@@ -24,6 +25,23 @@ func _ready():
 		var world_dict : Dictionary = parser.parse_file(filename)
 		fill_room(world_dict, start)
 
+# This function is a check for a range of 3 tiles on two tiles distance of the given
+# (x, z) coordinates.
+# A small sketch:
+# . c c c .
+# c . . . c
+# c . x . c
+# c . . . c
+# . c c c .
+# If the given x needs to be checked, if any c is a wall, the function will
+# return false.
+# The vertical flag checks the upper and lower row, while setting it to false
+# means the function will check the two columns.
+# Setting the orientation to 1, will mean the function will check the
+# positive side of the given direction (lower row if vertical flag, right if
+# the flag is not set), while setting the orientation to
+# -1 will result in the function checking the negative side (upper with flag,
+# left without).
 func wall_check(floor_plan: Array, x: int, z: int, max_x: int, max_z: int, orientation: int, is_vertical: bool) -> bool:
 	if is_vertical:
 		if x > 0 and floor_plan[x - 1][z + 2 * orientation]:
@@ -41,6 +59,12 @@ func wall_check(floor_plan: Array, x: int, z: int, max_x: int, max_z: int, orien
 			return false
 	return true
 
+# This function checks if the wall that gets placed at the given (x, z)
+# coordinates can be safely placed. Part of this is the check if it's inside
+# the room. Another part is the check if it does not create a corridor that is
+# too small for the player to navigate. Only if all the checks are fine, does
+# this function return true.
+# This function does not check if there exists a route from start to finish.
 func check_wall_placement(floor_plan: Array, x: int, z: int) -> bool:
 	var max_x: int = floor_plan.size()
 	var max_z: int = floor_plan[0].size()
@@ -48,6 +72,11 @@ func check_wall_placement(floor_plan: Array, x: int, z: int) -> bool:
 	if x < 0 or x >= max_x or z < 0 or z >= max_z:
 		return false
 
+	if floor_plan[x][z]:
+		return false
+
+	# If there is a hole next to the coordinates, only place the wall if there
+	# is no wall on two tiles distance, since the player can't pass then.
 	if z > 1 and not floor_plan[x][z - 1]:
 		if not wall_check(floor_plan, x, z, max_x, max_z, -1, true):
 			return false
@@ -63,7 +92,13 @@ func check_wall_placement(floor_plan: Array, x: int, z: int) -> bool:
 
 	return true
 
-func place_wall(x: int, z: int, i: int, orientation: int, floor_plan: Array):
+# This function is a helper function for the handle_wall function.
+# This function actually tries to place the wall at the x(+ i), z( + i)
+# coordinates and will update the floor plan if the wall can be placed.
+# The exact location depends on the given i and the orientation.
+# This check is done by check_wall_placement. If the wall was placed this
+# function returns true, and false if the wall could not be placed.
+func place_wall(x: int, z: int, i: int, orientation: int, floor_plan: Array) -> bool:
 	var wall_block = wall_scene.instantiate()
 	var new_x = x
 	var new_z = z
@@ -78,7 +113,12 @@ func place_wall(x: int, z: int, i: int, orientation: int, floor_plan: Array):
 	add_child(wall_block, true)
 	return true
 
-func handle_wall(floor_plan : Array, wall : Dictionary, width : int, height: int, start : Vector3i):
+# This function will try to fit a wall on the floor plan given the restrictions
+# of the wall dictionary, room size and start position.
+# If the operation is successful, the floor plan will be updated, but if not
+# a single tile of the wall can be placed, this function will not update the
+# floor plan, and will return false.
+func handle_wall(floor_plan : Array, wall : Dictionary, width : int, height: int, start : Vector3i) -> bool:
 	var placed : bool = false
 	var min_dist : int  = wall['set_min_distance']
 	var max_dist : int = wall['set_max_distance']
@@ -112,10 +152,14 @@ func handle_wall(floor_plan : Array, wall : Dictionary, width : int, height: int
 			xmin = 1
 		else:
 			xmin = start[0] + min_dist
+		orientation = VERTICAL
 		
 		xmax = min(width - 2, start[0] + max_dist)
 	else:
+		# The length is too large to fit horizontally or vertically, so cancel the operation.
 		return false
+
+	# Get a random x coordinate inside the bounds.
 	x = randi_range(xmin, xmax)
 	for i in range(-length / 2, length / 2):
 		if place_wall(x, z, i, orientation, floor_plan):
@@ -126,6 +170,9 @@ func handle_wall(floor_plan : Array, wall : Dictionary, width : int, height: int
 			placed = true
 	return placed
 
+# This is a small function that relies on the helper functions to place the walls.
+# If the first try of placing a wall fails, it will give a second try,
+# but no more than 2.
 func add_walls(floor_plan : Array[Array], wall_list : Array, width : int, height : int, start : Vector3i) -> void:
 	for wall in wall_list:
 		if not handle_wall(floor_plan, wall, width, height, start):
@@ -161,15 +208,22 @@ func add_item(floor_plan : Array[Array], object : Dictionary, width: int, height
 	add_child(item, true)
 	return true
 
+
+func object_matcher(object : Dictionary, floor_plan : Array[Array], width : int, height : int, start: Vector3i) -> bool:
+	match object['type']:
+		'ITEM':
+			return add_item(floor_plan, object, width, height, start)
+		_:
+			print('The object is not an item')
+			return true
+
+# This function will eventually handle all the object placements. Currently it
+# only support items and it will handle objects that failed to place.
 func add_objects(floor_plan : Array[Array], objects_list : Array, width : int, height: int, start: Vector3i):
 	for object in objects_list:
-		match object['type']:
-			'ITEM':
-				if not add_item(floor_plan, object, width, height, start):
-					# If the placement was unsuccessful, try once more.
-					add_item(floor_plan, object, width, height, start)
-			_:
-				print('The object is not an item')
+		if not object_matcher(object, floor_plan, width, height, start):
+			# Try again if failed the first time.
+			object_matcher(object, floor_plan, width, height, start)
 
 # Generates a matrix of the size (width, height)
 func generate_floor_plan(width : int, height : int) -> Array[Array]:
@@ -213,6 +267,3 @@ func fill_room(world_dict: Dictionary, start : Vector3i):
 	button2.position = Vector3i(randi_range(1, room[0] * 2 - 1), 2, randi_range(1, room[1] * 2 - 1))
 	add_child(button2)
 	button2.interactable = door
-
-#func _process(delta):
-	#pass
