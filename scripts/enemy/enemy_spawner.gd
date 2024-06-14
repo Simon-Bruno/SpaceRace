5c78776db4d7a7c5dcbd2f1652deb13d7c9059f4
Lucas Muntinga <lucas.muntinga@student.uva.nl>
2024-06-14 14:14:15 +0200

    Stupid oopsie

--- scripts/enemy/enemy_spawner.gd
+++ scripts/enemy/enemy_spawner.gd
@@ -5,10 +5,4 @@ func _enter_tree():
 		$"EnemyMultiplayerSpawner".set_multiplayer_authority(multiplayer.get_unique_id())
 		
 func _ready():
-	#var enemy = preload("res://scenes/enemy/enemy.tscn").instantiate()
-	#enemy.position = Vector3(2,20,4)
-	#add_child(enemy, true)
-
-	var ranged = preload("res://scenes/characters/ranged_enemy/ranged_enemy.tscn").instantiate()
-	ranged.position = Vector3(2,20,4)
-	add_child(ranged, true)
+	GlobalSpawner.spawn_ranged_enemy(Vector3(2,20,4))
