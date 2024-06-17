006b29d711d2cde6c2a747303f9a27f9222a0155
Raven Timmer <61437474+RavenTimmer@users.noreply.github.com>
2024-06-17 13:33:08 +0200

    feat: Added enemy model

--- scripts/world/customRoomDefs.gd
+++ scripts/world/customRoomDefs.gd
@@ -2,18 +2,19 @@ extends Node
 
 @onready var customRooms = preload("res://scenes/world/customRooms.tscn").instantiate()
 
-var rooms = ["Room1", "Room2"]
+var rooms = ["Room0", "Room1"]
 var roomNodes = []
+
+
+func get_room_item(location : Vector3i, room : int, layer : int):
+	
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for room in rooms:
 		roomNodes.append(customRooms.get_node(room))
-	
-	for i in roomNodes:
-		print(i.get_child(0).get_used_cells())
-
-
+		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
