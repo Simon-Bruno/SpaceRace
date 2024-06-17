fe2367cc8f47d917fd357c8ce90faad81e19737e
BasVanGeest <Bdvangeest@gmail.com>
2024-06-17 16:19:33 +0200

    feat: quick heal ability scientist

--- scenes/classes/Scientist.gd
+++ scenes/classes/Scientist.gd
@@ -1,12 +1,16 @@
 extends Node
 
+@onready var player_node = get_parent()
 
 var ability1_point_cost : int = 10
 var ability2_point_cost : int = 20
+var healing_amount : int = 50
 
-func ability1():
-	pass
+func ability1() -> void:
+	heal()
 	
-func ability2():
+func ability2() -> void:
 	pass
 
+func heal() -> void:
+	player_node.health =  min(Global.player_max_health, player_node.health + healing_amount)
