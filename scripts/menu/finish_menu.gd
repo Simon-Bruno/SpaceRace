36d39d9a566476b4be50788df921a76fccc2b4f1
Sytze <sytze.m9@gmail.com>
2024-06-17 14:24:58 +0200

    improved finish scene

--- scripts/menu/finish_menu.gd
+++ scripts/menu/finish_menu.gd
@@ -5,19 +5,15 @@ func _ready():
 	var username = Network.playername
 	var pressure_plate_vals = get_parent()
 	var winner_id = pressure_plate_vals.winner_id
-	
-	print("winner id is:", winner_id)
-	print("with his id is", multiplayer.get_unique_id())
-	
+
 	if multiplayer.get_unique_id() == (winner_id) or Network.other_team_member_id == (winner_id):
-		#print("this is a winners_id: ", multiplayer.get_unique_id())
 		var label_node = get_node("VBoxContainer/Label")
 		label_node.text = str(username + "\nYou won!")
+
 	else:
 		#print("this is a losers_id: ", multiplayer.get_unique_id())
 		var label_node = get_node("VBoxContainer/Label")
 		label_node.text = str(username + "\nYou lost!")
-	print("end of playermenu \n\n")
 	
 # Go to lobby
 func _on_play_again_pressed():
