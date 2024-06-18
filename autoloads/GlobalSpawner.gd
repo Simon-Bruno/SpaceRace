23e86a9b2d90f6193b7b3aaabe1f6e6ff25563b7
tessawernars <tessa.wernars@student.uva.nl>
2024-06-18 14:28:41 +0200

    added room

--- autoloads/GlobalSpawner.gd
+++ autoloads/GlobalSpawner.gd
@@ -6,30 +6,49 @@ var laser_scene = preload("res://scenes/interactables/laser.tscn")
 var item_scene = preload("res://scenes/item/item.tscn")
 var box_scene = preload("res://scenes/interactables/moveable_object.tscn")
 var button_scene = preload("res://scenes/interactables/button.tscn")
+var door_scene = preload("res://scenes/interactables/door.tscn")
 var pressure_plate_scene = preload("res://scenes/interactables/pressure_plate.tscn")
 var boss_scene = preload("res://scenes/characters/boss.tscn")
 var projectile_scene = preload("res://scenes/characters/ranged_enemy/projectile.tscn")
 
-func spawn_pressure_plate_enemy(pos):
+func spawn_pressure_plate(pos, dir, interact):
 	if not multiplayer.is_server():
 		return
 	var spawner = get_node_or_null("/root/Main/SpawnedItems/World/InteractableSpawner")
 	if spawner:
 		var plate = pressure_plate_scene.instantiate()
 		plate.position = pos
+		plate.basis	= dir
+		plate.interactable = interact
 		spawner.add_child(plate, true)
 		return plate
 	return null
 
-func spawn_button_enemy(pos):
+func spawn_button(pos, dir, interact, inverse):
 	if not multiplayer.is_server():
 		return
 	var spawner = get_node_or_null("/root/Main/SpawnedItems/World/InteractableSpawner")
 	if spawner:
 		var button = button_scene.instantiate()
 		button.position = pos
+		button.basis	= dir
+		button.interactable = interact
+		button.inverse = inverse
 		spawner.add_child(button, true)
 		return button
+	return null
+
+func spawn_door(pos, dir, activation):
+	if not multiplayer.is_server():
+		return
+	var spawner = get_node_or_null("/root/Main/SpawnedItems/World/InteractableSpawner")
+	if spawner:
+		var door = door_scene.instantiate()
+		door.position = pos
+		door.basis	= dir
+		door.activation_count = activation
+		spawner.add_child(door, true)
+		return door
 	return null
 
 func spawn_melee_enemy(pos):
