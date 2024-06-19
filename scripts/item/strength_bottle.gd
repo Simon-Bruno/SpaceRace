921944e87fb30b7bef1bd5a2ef5e4a91e801b06c
Milou <milouhermes@outlook.com>
2024-06-19 19:09:18 +0200

    Small correction timer strength bottle

--- scripts/item/strength_bottle.gd
+++ scripts/item/strength_bottle.gd
@@ -2,18 +2,13 @@ extends "res://scripts/item/item.gd"
 
 @onready var timer = $PotionTimer
 var strength_boost = 2
-var duration = 5
 var player : Node3D = null
-
-
-func _on_ready():
-	timer.timeout.connect(_on_potion_timer_timeout)
 
 
 # Called when the player actually uses the item by pressing KEY 'Q'
 func use():
 	# Start timer for potion effect
-	timer.start(duration) 
+	timer.start() 
 	player = owned_node
 	if player:
 		player.strength = strength_boost
