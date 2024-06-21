a82c6f5c9977b602ecf2fc572c27aef9d4101de4
Raven Timmer <timmerraven@gmail.com>
2024-06-21 11:26:42 +0200

    fix: Fixed crashing when drawing paths

--- scripts/world/customRoomDefs.gd
+++ scripts/world/customRoomDefs.gd
@@ -12,19 +12,19 @@ var special_roomNodes = []
 
 
 func get_room_item(location : Vector3i, room : int, layer : int, special : bool) -> int:
-	#var nodes = special_roomNodes if special else roomNodes
-	return roomNodes[room].get_child(layer).get_cell_item(location)
+	var nodes = special_roomNodes if special else roomNodes
+	return nodes[room].get_child(layer).get_cell_item(location)
 
 
 func get_room_item_orientation(location : Vector3i, room : int, layer : int, special : bool) -> int:
-	#var nodes = special_roomNodes if special else roomNodes
-	return roomNodes[room].get_child(layer).get_cell_item_orientation(location)
+	var nodes = special_roomNodes if special else roomNodes
+	return nodes[room].get_child(layer).get_cell_item_orientation(location)
 
 
 func get_room_size(room : int, special : bool) -> Array:
-	#var nodes = special_roomNodes if special else roomNodes
-	var start = roomNodes[room].get_child(0).get_used_cells_by_item(START)[0]
-	var end = roomNodes[room].get_child(0).get_used_cells_by_item(END)[0]
+	var nodes = special_roomNodes if special else roomNodes
+	var start = nodes[room].get_child(0).get_used_cells_by_item(START)[0]
+	var end = nodes[room].get_child(0).get_used_cells_by_item(END)[0]
 
 	return [end.x - start.x - 1, end.z - start.z + 1, start.x + 1]
 
@@ -39,8 +39,8 @@ func _ready():
 	for room in rooms:
 		roomNodes.append(customRooms.get_node(room))
 
-	#for room in special_rooms:
-		#special_roomNodes.append(customRooms.get_node(room))
+	for room in special_rooms:
+		special_roomNodes.append(customRooms.get_node(room))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
