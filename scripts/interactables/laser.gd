56a463aa102a037486564c2e0b5dd2e441d983f5
Lucas Muntinga <lucas.muntinga@student.uva.nl>
2024-06-21 11:04:40 +0200

    Fixed even more:

--- scripts/interactables/laser.gd
+++ scripts/interactables/laser.gd
@@ -84,7 +84,7 @@ func _process(delta):
 
 	if target != null and active:
 		#target.die()
-		target.take_damage.rpc(target.name, 80)
+		target.take_damage.rpc(target.name, 1000)
 		
 	if not multiplayer.is_server():
 		return
