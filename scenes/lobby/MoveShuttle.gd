bcd176a03c1e6b9faa9e8c9f2b7a818533f18665
ertugrul kilic <kilicertugrul41@gmail.com>
2024-06-21 13:39:08 +0200

    Shuttles accelerates gradually towards the portal

--- scenes/lobby/MoveShuttle.gd
+++ scenes/lobby/MoveShuttle.gd
@@ -1,11 +1,21 @@
-extends MeshInstance3D
+extends Node3D
 
+# Initial speed
+var speed = 0.0
+
+# Acceleration rate (units per second squared)
+const ACCELERATION = 5.0
+
+@onready var mesh = $SpaceshuttleV2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	# Increase the speed by the acceleration multiplied by the delta time
+	speed += ACCELERATION * delta
+	
+	# Move the shuttle to the left
+	position += transform.basis * Vector3(-speed, speed/5, 0) * delta
