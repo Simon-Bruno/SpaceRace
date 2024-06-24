2fec9b279f1223ee2e308632732922964dc05935
Yifan Chen <yifan.chen2@student.uva.nl>
2024-06-24 11:28:57 +0200

    fix: owned items are now ignored as candidates entirely

--- scripts/world/roomGeneration.gd
+++ scripts/world/roomGeneration.gd
@@ -300,9 +300,9 @@ func add_enemy_laser(floor_plan : Array[Array], object : Dictionary, width : int
 	var orientation = orientations[randi() % orientations.size()]
 	var angle = deg_to_rad(orientation)
 	var basis = Basis().rotated(Vector3(0, 1, 0), angle)
-	var laser = GlobalSpawner.spawn_laser(absolute_position + Vector3i(x, 0, -z), basis, false, object['set_activation'], true, true)
+	var laser = GlobalSpawner.spawn_laser(absolute_position + Vector3(x, 0, -z), basis, false, object['set_activation'], true, true)
 	basis = Basis().rotated(Vector3(0, -1, 0), angle)
-	var laser2 = GlobalSpawner.spawn_laser(absolute_position + Vector3i(x, 0, z), basis, false, object['set_activation'], true, true)
+	var laser2 = GlobalSpawner.spawn_laser(absolute_position + Vector3(x, 0, z), basis, false, object['set_activation'], true, true)
 
 	for i in object['set_activation']:
 		var button_object = {'set_min_distance' : 3, 'set_max_distance' : 20}
