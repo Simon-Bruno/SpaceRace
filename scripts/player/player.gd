893cf70bc6d1f84c9fd6b7f267a571485e9dda9e
Raven Timmer <timmerraven@gmail.com>
2024-06-26 12:45:20 +0200

    Playtest: Changed some maps

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -30,7 +30,7 @@ var AnimJump: bool = false
 @onready var HpBar = $PlayerCombat/SubViewport/HpBar
 
 var lobby_spawn = Vector3(0, 11, 20)
-var game_spawn = {1: [Vector3(10, 5, 5), Vector3(10, 5, 10)],2: [Vector3(10, 5, -5), Vector3(10, 5, -10)]}
+var game_spawn = {1: [Vector3(3, 3, 5), Vector3(3, 3, 11)],2: [Vector3(4, 3, -4), Vector3(4, 3, 10)]}
 
 func _enter_tree():
 	$MultiplayerSynchronizer.set_multiplayer_authority(str(name).to_int())
