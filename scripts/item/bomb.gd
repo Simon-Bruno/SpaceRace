dba0d6c9282f4f77f06300840a5acc4c762b4a9f
Milou <milouhermes@outlook.com>
2024-06-26 10:26:15 +0200

    Added networking logic for bomb

--- scripts/item/bomb.gd
+++ scripts/item/bomb.gd
@@ -4,22 +4,23 @@ extends "res://scripts/item/item.gd"
 @onready var blast_radius_visual = $RigidBody3D/Radius/VisualBlastRadius
 
 var bomb_damage = 50
-var player : Node3D = null
 
 
 func _on_ready():
+	# Hide the blast radius in the beginning
 	blast_radius_visual.visible = false
 
 
 func use():
 	# Start timer after pressing "Q" when holding the bomb item
 	timer.start()
+	# When bomb is used, show the area that it will impact
 	blast_radius_visual.visible = true
-	player = Network.get_player_node_by_id(owned_id)
 
 
 @rpc("any_peer", "call_local", "reliable")
 func _bomb_explode():
+	var player = Network.get_player_node_by_id(owned_id)
 	var blast_radius = $RigidBody3D/Radius
 	# Loop over the bodies that are within the blast radius and give damage
 	for target in blast_radius.get_overlapping_bodies():
@@ -29,9 +30,11 @@ func _bomb_explode():
 				target.take_damage(target.name, bomb_damage)
 			elif target.is_in_group("Enemies"):
 				# damage, player position
-				target.take_damage(bomb_damage, player.global_transform.origin)
-			elif target.is_in_group("Boss"):
-				print("Boss")
+				if player:
+					target.take_damage(bomb_damage, player.global_transform.origin)
+				else:
+					print("Error: player is null during explosion")
+			#elif target.is_in_group("Boss"):
 				# TODO: Doesn't work for now, since the boss isn't available for testing yet
 				#target.take_damage(bomb_damage, player.global_transform.origin)		
 	super.delete()
@@ -39,7 +42,7 @@ func _bomb_explode():
 
 # The bomb will explode after three seconds
 func _on_fuse_timer_timeout():
-	_bomb_explode()
+	_bomb_explode.rpc()
 	blast_radius_visual.visible = false
 	
 	
