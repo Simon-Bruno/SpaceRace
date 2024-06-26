931a7463fa3508d9ba19779fbe7105fedefd6ac5
Milou <milouhermes@outlook.com>
2024-06-26 12:11:28 +0200

    Small correction

--- scripts/item/item.gd
+++ scripts/item/item.gd
@@ -88,6 +88,5 @@ func _process(delta):
 		item_position = $RigidBody3D.global_position.lerp(destination, item_follow_speed * delta)
 		$RigidBody3D.global_position = item_position
 		var player = owned_node.get_node("PlayerItem")
-		if !owned_node.alive:
-			if player.holding:
-				player._drop_item() # When the player dies and is holding an item, drop it
+		if !owned_node.alive and player.holding:
+			player._drop_item() # When the player dies and is holding an item, drop it
