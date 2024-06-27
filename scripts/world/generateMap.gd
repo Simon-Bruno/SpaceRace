619b5dcb035b4b9a1ac06d70c569b4080bfdddb5
Simon Bruno <simonadrbruno@gmail.com>
2024-06-27 13:14:46 +0200

    jump reverted

--- scripts/world/generateMap.gd
+++ scripts/world/generateMap.gd
@@ -36,7 +36,7 @@ const PAIRS: Dictionary = {DOOROPENL: DOOROPENR, DOOROPENR: DOOROPENL, DOORCLOSE
 							DOORCLOSEDR: DOORCLOSEDL, WINDOWR: WINDOWL, WINDOWL: WINDOWR}
 
 # What percentage of the rooms should be custom.
-const CUSTOMROOMPERCENTAGE : float = 1
+const CUSTOMROOMPERCENTAGE : float = 0.65
 
 # General room parameters
 const room_amount: int = 8
