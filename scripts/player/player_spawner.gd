041c270995441a4085a43a9b6c8edd8fdce62dc4
Michael Peter Antonius van den Broek <michael.van.den.broek@student.uva.nl>
2024-06-27 11:14:54 +0200

    reimplemented respawn immunity. few minor adjustments to respawn logic.

--- scripts/player/player_spawner.gd
+++ scripts/player/player_spawner.gd
@@ -1,19 +1,19 @@
 extends Node
 
-
 func add_player_character(id):
 	var character = preload("res://scenes/player/player.tscn").instantiate()
 	character.add_to_group("Players")
 	character.name = str(id)
 	add_child(character)
 
-
+var dead_player_y_value: int = 0
 var dead_player = null
 var fall_acceleration = 0
 
 func player_died(player_to_die):
 	player_to_die.alive = false
 	player_to_die.visible = false
+	dead_player_y_value = player_to_die.global_position.y
 	player_to_die.global_position.y += 200
 	fall_acceleration = player_to_die.fall_acceleration	
 	player_to_die.fall_acceleration = 0
@@ -26,9 +26,12 @@ func respawn_player():
 	player.get_node("PlayerCombat/SubViewport/HpBar").value = Global.player_max_health
 	player.alive = true
 	player.visible = true
-	player.global_position.y -= 199
+	if dead_player_y_value != 0:
+		player.global_position.y = dead_player_y_value + 1
+	#player.global_position.y -= 199
 	player.fall_acceleration = fall_acceleration
 	player.get_node("./PlayerCombat/RespawnImmunity").start()
+	player.respawn_immunity = true
 
 func _on_respawn_timer_timeout():
 	respawn_player()
