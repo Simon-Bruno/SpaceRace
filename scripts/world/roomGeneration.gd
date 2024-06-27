b65a5095504999100e7d7839478efb3e919890a8
wesselpierik <wessel.pierik@student.uva.nl>
2024-06-27 14:13:15 +0200

    chore: Removed the hardcoded room

--- scripts/world/roomGeneration.gd
+++ scripts/world/roomGeneration.gd
@@ -31,7 +31,6 @@ func _ready():
 					filenames.append(file)
 				file = dir.get_next()
 		var filename = filenames[randi() % filenames.size()]
-		filename = "hinder.rms"
 		var world_dict : Dictionary = parser.parse_file("res://files/random_map_scripts/" +  filename)
 		fill_room(world_dict, start, end, last_room)
 
