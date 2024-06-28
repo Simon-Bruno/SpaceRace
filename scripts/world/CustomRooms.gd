3c69e3ea7b0e4043699b1bfff98631da0e3ac1dd
tessawernars <tessa.wernars@student.uva.nl>
2024-06-28 13:49:52 +0200

    added comments to entity generation

--- scripts/world/CustomRooms.gd
+++ scripts/world/CustomRooms.gd
@@ -10,26 +10,6 @@ static var END = 23
 @export var rooms = []
 @export var end_room = []
 
-#var interactables = {
-	#0 : {},
-	#1 : {},
-	#2 : {
-#[Vector3(1,1,10), Vector3(1,1,11)] : Vector3(1,1,6),
-#[Vector3(1,1,10), Vector3(1,1,11)] : Vector3(2,1,12),
-#
-#[Vector3(4,1,9), Vector3(5,1,9)] : Vector3(6,1,11),
-#[Vector3(4,1,9), Vector3(5,1,9)] : Vector3(6,1,8),
-#
-#[Vector3(3,1,9), Vector3(4,1,9)] : Vector3(6,1,5),
-#[Vector3(3,1,9), Vector3(4,1,9)] : Vector3(2,1,3),
-#
-#[Vector3(4,1,2), Vector3(4,1,3)] : Vector3(1,1,1),
-#[Vector3(4,1,2), Vector3(4,1,3)] : Vector3(6,1,1),
-#},
-	#3 : {
-#[Vector3(3,1,1), Vector3(3,1,2)] : [Vector3(1,1,0), Vector3(0,1,6), Vector3(1,1,7), Vector3(3,1,5)]
-#}
-	#}
 
 # Sorts vectors based on x value.
 func sort_vector(a : Vector3i, b : Vector3i):
