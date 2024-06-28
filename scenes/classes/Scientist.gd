90845e4e23e9aaf62249b366294dd9681d025c07
Bas van Geest <bdvangeest@gmail.com>
2024-06-28 13:50:21 +0200

    ciheio

--- scenes/classes/Scientist.gd
+++ scenes/classes/Scientist.gd
@@ -1,69 +1,50 @@
 extends Node
 
-@onready var weaponDurationTimer = $weaponDuration
-@onready var strenthDurationTimer = $strengthDuration
 @onready var player_node = get_parent()
-@onready var player_combat_node = player_node.get_node("PlayerCombat")
 
-var pistol = preload("res://scenes/weapons/pistol.tscn")
-var fists = preload("res://scenes/weapons/fists.tscn")
-
-var strength_debuff : float = 0.5
+var ability1_point_cost : int = 10
+var ability2_point_cost : int = 20
+var healing_amount : int = 50
 
 var ability1_cooldown : int = 20
-var ability2_cooldown : int = 40
+var ability2_cooldown : int = 30
 
-var ability1_title : String = "Strength buff"
-var ability2_title : String = "Upgrade weapon"
+var ability1_title : String = "Heal yourself"
+var ability2_title : String = "Slow opponents"
 
-# This functions gets called once when the node enters the scene tree.
-func _ready(): 
-	# Load HUD
+# This functions gets called once when the node enters the scene tree
+func _ready():
+	# load the HUD
 	var hud = get_node_or_null("../../../HUD")
 	if hud:
 		hud.set_ability_info(ability1_title, ability2_title, ability1_cooldown, ability2_cooldown)
 
-func ability1():
-	print("ability 1 soldier")
-	strength_buff()
-	
-func ability2():
-	print("ability 2 soldier")
-	pistol_upgrade()
 
-func pistol_upgrade():
-	# get the current weapon and remove it, start the tinmer for the ability duration
-	var weapon_node = player_node.get_node("PlayerCombat/Weapon")
-	weaponDurationTimer.start()
-	player_combat_node.remove_child(weapon_node)
+func ability1() -> void:
+	print("ability 1 scientist")
+	heal()
 	
-	# load the pistol node and add it to the same spot as the previous weapon
-	var new_pistol = pistol.instantiate()
-	player_combat_node.add_child(new_pistol, true)
-	new_pistol.set_name("Weapon") 
+func ability2() -> void:
+	print("ability 2 scientist")
+	slow_enemies()
+
+func slow_enemies() -> void:
+	set_slowness_on_other_team.rpc(Network.get_other_team_ids(multiplayer.get_unique_id()))
+	
+@rpc("any_peer", "call_local", "reliable")
+func set_slowness_on_other_team(ids):
+	# only execute the code when the node is equal to the actual peer running the code
+	if ids.has(str(multiplayer.get_unique_id())):
+		Network.get_player_node_by_id(multiplayer.get_unique_id()).walkspeed_multiplier = 0.2
+		await get_tree().create_timer(6).timeout # slow duration
+		Network.get_player_node_by_id(multiplayer.get_unique_id()).walkspeed_multiplier = 1
+
+func heal() -> void:
+	# make sure the health cant go above the Global.player_max_health
+	get_parent().health =  min(Global.player_max_health, get_parent().health + healing_amount)
+	get_parent().HpBar.value = get_parent().health
 	
 	# Display ability message
-	%Label.text = "+ pistol"
+	%Label.text = "+ health"
 	await get_tree().create_timer(1.0).timeout
 	%Label.text = ""
-	
-# this function only gets called when the weapon duration timer reaches its end
-func _on_weapon_duration_timeout():
-	# Remove the pistol node
-	var weapon_node = player_node.get_node("PlayerCombat/Weapon")
-	player_combat_node.remove_child(weapon_node)
-	player_combat_node.add_child(fists.instantiate(), true)
-	
-func strength_buff():
-	# update the strength value. This value multiplies the damage output of the player
-	player_node.strength += strength_debuff
-	strenthDurationTimer.start()
-	
-	# Display ability message
-	%Label.text = "+ strength"
-	await get_tree().create_timer(1.0).timeout
-	%Label.text = ""
-
-# this function only gets called when the strength duration timer reaches its end.
-func _on_strength_duration_timeout():
-	player_node.strength -= strength_debuff
